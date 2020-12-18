a7e4c0c00f943e9a66110dcc0e4b5ec18cf5b18b
kizrain <kizrain@hotmail.com>
2020-12-18 11:29:29 +0900

    first commit

---
+++ src/test/java/me/sunee/leafTest/LeafTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sunee.leafTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
